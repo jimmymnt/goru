cf712abb95015e762fb533477560cc4a50b8b30d
jimmy.nguyen <tuannguyen@2fsoft.info>
2024-03-18 20:59:42 +0700

    printf and capture value from input terminal

--- hello.go
+++ hello.go
@@ -3,11 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	var confrerenceName = "Go Confrerence"
+	confrerenceName := "Go Confrerence"
 	const confrerenceTickets = 50
-	var confrerenceRemains = 50
+	confrerenceRemains := 50
 
 	fmt.Printf("Welcome to our %v booking application\n", confrerenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", confrerenceTickets, confrerenceRemains)
 	fmt.Printf("Get your tickets here to attend.\n")
+
+	for {
+		var firstName string
+		var lastName string
+		var userTickest int
+
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter number of tickets:")
+		fmt.Scan(&userTickest)
+
+		fmt.Printf("Your fullname is %v.\n", firstName+" "+lastName)
+		fmt.Printf("You have booked %v tickets.\n", userTickest)
+
+		confrerenceRemains -= userTickest
+		fmt.Printf("Now, we have %v tickets remain.\n", confrerenceRemains)
+	}
 }
